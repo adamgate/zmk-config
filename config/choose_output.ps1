ec3c865abf43917bcadec091625593574bcc305d
adamgate <applegateadaml@gmail.com>
2023-06-09 16:22:49 -0600

    added pwshl script to load config

---
+++ config/choose_output.ps1
@@ -0,0 +1,15 @@
+$Folder = Read-Host "Please enter the folder of the keymap you want to build" 
+
+if (Test-Path -Path $Folder) {
+    # See if the files are already loaded.
+    if ((Get-FileHash 'corne.conf').Hash -eq (Get-FileHash ".\${Folder}\corne.conf").Hash -and
+        (Get-FileHash 'corne.keymap').Hash -eq (Get-FileHash ".\${Folder}\corne.keymap").Hash -and
+        (Get-FileHash 'west.yml').Hash -eq (Get-FileHash ".\${Folder}\west.yml").Hash) {
+            return Write-Host "The given keymap is already loaded. Exiting..." -ForegroundColor Blue
+        }
+
+    Copy-Item -Path ".\${Folder}\*" -Destination . -Force -Include *.conf,*.keymap,*.yml
+    Write-Host "Successfully set keymap to build." -ForegroundColor Green
+} else {
+    Write-Host "Couldn't find the keymap folder '${Folder}'." -ForegroundColor Red
+}
