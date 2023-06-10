260a11f070142dcd9cd947e65d51ea7ad433792a
adamgate <applegateadaml@gmail.com>
2023-06-09 21:14:25 -0600

    Added export functionality to powershell script

--- config/stage_keymap.ps1
+++ config/stage_keymap.ps1
@@ -1,18 +1,39 @@
 # A simple script to make switching between layouts painless
+# also exports config to my dotfiles repo
 
-$Folder = Read-Host "Please enter the name of the folder containing the keymap you want to build (e.g. 'default')" 
+# Exports the entire repo except for github actions to the target directory
+function Export-Dotfiles {
+    # $exportPath = "${HOME}\Code\Personal\config-files\keyboards\corne\"
+    $exportPath = "E:\Code\Personal\config-files\keyboards\corne\"
+    if (-not (Test-Path -Path $exportPath)) {
+        return Write-Host "Couldn't find the export path. Please make sure you have the dotfiles repo cloned in the corret spot. `n" -ForegroundColor Red
+    }
 
-# Ensure provided folder exists
-if (Test-Path -Path $Folder) {
-    # See if the files are already loaded.
-    if ((Get-FileHash 'corne.conf').Hash -eq (Get-FileHash ".\${Folder}\corne.conf").Hash -and
-        (Get-FileHash 'corne.keymap').Hash -eq (Get-FileHash ".\${Folder}\corne.keymap").Hash -and
-        (Get-FileHash 'west.yml').Hash -eq (Get-FileHash ".\${Folder}\west.yml").Hash) {
-            return Write-Host "The keymap '${Folder}' is already loaded. Exiting..." -ForegroundColor Blue
-        }
-
-    Copy-Item -Path ".\${Folder}\*" -Destination . -Force -Include *.conf,*.keymap,*.yml
-    Write-Host "Successfully staged keymap '${Folder}' to build." -ForegroundColor Green
-} else {
-    Write-Host "Couldn't find the keymap folder '${Folder}' for staging." -ForegroundColor Red
+    Write-Host "Exporting repository..."
+    Copy-Item -Path "..\*" -Destination $exportPath -Exclude .github,.git -Force -Recurse
+    Write-Host "Repository exported successfully." -ForegroundColor Green
 }
+
+# Loads the selected keymap into the directory to be built by github actions
+function Stage-Keymap {
+    $Folder = Read-Host "Please enter the name of the folder containing the keymap you want to build (e.g. 'default')" 
+
+    # Ensure provided folder exists
+    if (-not (Test-Path -Path $Folder)) {
+        return Write-Host "Couldn't find the keymap folder '${Folder}' for staging. `n" -ForegroundColor Red
+    }
+
+    # See if the files are already loaded.
+    if ((Get-FileHash "corne.conf").Hash -eq (Get-FileHash ".\${Folder}\corne.conf").Hash -and
+        (Get-FileHash "corne.keymap").Hash -eq (Get-FileHash ".\${Folder}\corne.keymap").Hash -and
+        (Get-FileHash "west.yml").Hash -eq (Get-FileHash ".\${Folder}\west.yml").Hash) {
+            return Write-Host "The keymap '${Folder}' is already staged. Exiting... `n" -ForegroundColor Blue
+        }
+    
+    Copy-Item -Path ".\${Folder}\*" -Destination . -Force -Include *.conf,*.keymap,*.yml
+    Write-Host "Successfully staged keymap '${Folder}' to build. `n" -ForegroundColor Green
+}
+
+# Entry point for the program
+Export-Dotfiles
+Stage-Keymap
